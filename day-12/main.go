a8f83f618e5564ee4e360618959c5fac67d989db
Ryan Baldwin <rtbaldwin777@gmail.com>
2023-12-13 22:49:26 -0600

    Day 12

--- day-12/main.go
+++ day-12/main.go
@@ -1,3 +1,7 @@
+/*****
+* Solution was derived from https://www.reddit.com/r/adventofcode/comments/18hbbxe/2023_day_12python_stepbystep_tutorial_with_bonus/
+* I could not figure this out by myself :(
+*****/
 package main
 
 import (
@@ -13,7 +17,7 @@ func main() {
 }
 
 func partOne() {
-	f, err := os.Open("test.txt")
+	f, err := os.Open("input.txt")
 	if err != nil {
 		panic(err)
 	}
@@ -21,53 +25,148 @@ func partOne() {
 
 	scanner := bufio.NewScanner(f)
 
-	springs := make([]Input, 0)
+	total := 0
+	cache := make(map[string]int)
 
 	for scanner.Scan() {
 		split := strings.Split(scanner.Text(), " ")
-		numbers := make([]int, 0)
+		groups := make([]int, 0)
 		for _, val := range strings.Split(split[1], ",") {
 			num, err := strconv.Atoi(val)
 			if err != nil {
 				panic(err)
 			}
-			numbers = append(numbers, num)
+			groups = append(groups, num)
 		}
-		springs = append(springs, Input{split[0], numbers})
+		res := calc(split[0], groups, cache)
+		total += res
 	}
 
-	for _, input := range springs {
-		sets := make([]string, 0)
-		for _, str := range strings.Split(input.springs, ".") {
-			if str != "" {
-				sets = append(sets, str)
-			}
-		}
-		// total := 0
-		// for _, set := range sets {
-
-		// }
-	}
-
-	// fmt.Println(springs)
-
+	println(total)
 }
 
 func partTwo() {
-	// f, err := os.Open("input.txt")
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// defer f.Close()
+	f, err := os.Open("input.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
 
-	// scanner := bufio.NewScanner(f)
+	scanner := bufio.NewScanner(f)
 
-	// for scanner.Scan() {
-	// 	line := scanner.Text()
-	// }
+	total := 0
+	cache := make(map[string]int)
+
+	for scanner.Scan() {
+		split := strings.Split(scanner.Text(), " ")
+		groups := make([]int, 0)
+		for _, val := range strings.Split(split[1], ",") {
+			num, err := strconv.Atoi(val)
+			if err != nil {
+				panic(err)
+			}
+			groups = append(groups, num)
+		}
+		springs := split[0]
+		allGroups := make([]int, 0)
+		allGroups = append(allGroups, groups...)
+		for i := 0; i < 4; i++ {
+			springs += "?" + split[0]
+			allGroups = append(allGroups, groups...)
+		}
+		res := calc(springs, allGroups, cache)
+		total += res
+	}
+
+	println(total)
+}
+
+func calc(springs string, groups []int, cache map[string]int) int {
+	cacheKey := createCacheKey(springs, groups)
+	val, exists := cache[cacheKey]
+	if exists {
+		return val
+	}
+
+	if len(groups) == 0 {
+		if !strings.Contains(springs, "#") {
+			cache[cacheKey] = 1
+			return 1
+		} else {
+			cache[cacheKey] = 0
+			return 0
+		}
+	}
+
+	if len(springs) == 0 {
+		cache[cacheKey] = 0
+		return 0
+	}
+
+	nextChar := springs[0]
+	nextGroup := groups[0]
+
+	out := 0
+	if nextChar == '#' {
+		out = pound(springs, groups, nextGroup, cache)
+	} else if nextChar == '.' {
+		out = dot(springs, groups, cache)
+	} else if nextChar == '?' {
+		out = dot(springs, groups, cache) + pound(springs, groups, nextGroup, cache)
+	} else {
+		panic("An invalid character was read")
+	}
+	cache[cacheKey] = out
+	return out
+}
+
+func pound(springs string, groups []int, nextGroup int, cache map[string]int) int {
+	if len(springs) < nextGroup {
+		return 0
+	}
+	currentGroup := springs[:nextGroup]
+	currentGroup = strings.ReplaceAll(currentGroup, "?", "#")
+
+	if currentGroup != fillString("#", nextGroup) {
+		return 0
+	}
+
+	if len(springs) == nextGroup {
+		if len(groups) == 1 {
+			return 1
+		} else {
+			return 0
+		}
+	}
+
+	if springs[nextGroup] == '?' || springs[nextGroup] == '.' {
+		return calc(springs[nextGroup+1:], groups[1:], cache)
+	}
+
+	return 0
+}
+
+func dot(springs string, groups []int, cache map[string]int) int {
+	return calc(springs[1:], groups, cache)
+}
+
+func fillString(char string, length int) string {
+	str := ""
+	for i := 0; i < length; i++ {
+		str += char
+	}
+	return str
+}
+
+func createCacheKey(springs string, groups []int) string {
+	key := springs
+	for _, group := range groups {
+		key += string(group) // I don't know why this works but strconv gives the wrong answer
+	}
+	return key
 }
 
 type Input struct {
 	springs string
-	numbers []int
+	groups  []int
 }
